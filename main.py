6bb9c38bb2861748fcb89a64c7512b3a373391ae
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-18 16:38:51 -0700

    added additional logic to main init function

--- main.py
+++ main.py
@@ -9,30 +9,36 @@ class Main(tk.Tk):
 
         self.encounter_text = []
 
-        initial_instructions = tk.Label(self, text="This is where the backstory will show.", bg="lightgrey", fg="black", pady=10)
-
-        self.encounter_text.append(initial_instructions)
-
-        for text in self.encounter_text:
-            text.pack(side=tk.TOP, fill=tk.X)
-
-        self.text_create = tk.Text(self, height=3, bg="white", fg="black")
-        self.text_create.pack(side=tk.BOTTOM, fill=tk.X)
-        self.text_create.focus_set()
-
-        self.bind("<Return>", self.next())
-
         self.game_canvas= tk.Canvas(self)       # background canvas for the game
-
         self.story_frame = tk.Frame(self.game_canvas)
 
         self.scrollbar = tk.Scrollbar(self.game_canvas, orient="vertical", command=self.game_canvas.yview)
+
         self.game_canvas.configure(yscrolcommand=self.scrollbar.set)
 
         self.title("Seven Seats")
         self.geometry("640x640")
 
-        self.scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+        self.text_create = tk.Text(self.story_frame, height=3, bg="white", fg="black")
+
+        self.game_canvas.pack(side=tk.BOTTOM, fill=tk.X)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas_frame = self.game_canvas.create_window((0,0), window=self.story_frame, anchor="n")
+
+        self.text_create = tk.Text(self, height=3, bg="white", fg="black")
+        self.story_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.text_create.focus_set()
+
+        initial_instructions = tk.Label(self, text="This is where the backstory will show.", bg="lightgrey", fg="black", pady=10)
+        initial_instructions.bind("<Button-1>", self.remove_text)
+
+        self.encounter_text.append(initial_instructions)
+
+        self.bind("<Return>", self.next())
+
+        for text in self.encounter_text:
+            text.pack(side=tk.TOP, fill=tk.X)
 
     def next(self):
         for text in self.encounter_text:
